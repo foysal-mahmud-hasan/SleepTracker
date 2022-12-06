b537d037ff533eeb5739f951ab02adfbbd55d7ef
TatsuSolo <1730150@iub.edu.bd>
2022-12-06 19:32:37 +0600

    Initial-6/12/22

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -15,3 +15,25 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+// we annotate it with entity so that room can use this class to make table.
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+    //primary key
+    @PrimaryKey(autoGenerate = true)
+    var nightId : Long = 0L,
+
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli : Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "end_time_milli")
+    val endTimeMilli : Long = startTimeMilli,
+
+    @ColumnInfo(name = "quality_rating")
+    var sleepQuality : Int = -1
+)
